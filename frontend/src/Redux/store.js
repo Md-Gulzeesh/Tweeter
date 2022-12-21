faf5b53616fbb72f5f423db2813fc1a6c27e31c7
Md-Gulzeesh <gulzeesh2000@gmail.com>
2022-12-21 18:14:01 +0530

    removed redux devtool code

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -13,8 +13,7 @@ export const store = legacy_createStore(
   reducer,
   initState,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
